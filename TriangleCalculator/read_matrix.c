231c6e2f86bc25e9d8f8cad7eb12d7ff63632988
Billkyriaf <45237645+Billkyriaf@users.noreply.github.com>
2021-11-03 01:48:52 +0200

    Update read_matrix.c
    Add function that creates a matrix from the graph. At this point a sparse matrix is created so this is not ideal for larger matrixes

--- TriangleCalculator/read_matrix.c
+++ TriangleCalculator/read_matrix.c
@@ -1,38 +1,40 @@
-/*
-*   Matrix Market I/O example program
-*
-*   Read a real (non-complex) sparse matrix from a Matrix Market (v. 2.0) file.
-*   and copies it to stdout.  This porgram does nothing useful, but
-*   illustrates common usage of the Matrix Matrix I/O routines.
-*   (See http://math.nist.gov/MatrixMarket for details.)
-*
-*   Usage:  a.out [filename] > output
-*
-*
-*   NOTES:
-*
-*   1) Matrix Market files are always 1-based, i.e. the index of the first
-*      element of a matrix is (1,1), not (0,0) as in C.  ADJUST THESE
-*      OFFSETS ACCORDINGLY offsets accordingly when reading and writing
-*      to files.
-*
-*   2) ANSI C requires one to use the "l" format modifier when reading
-*      double precision floating point numbers in scanf() and
-*      its variants.  For example, use "%lf", "%lg", or "%le"
-*      when reading doubles, otherwise errors will occur.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "mmio.h"
 
+
+void createSparseArray(int *I, int *J, int m, int n, int nz){
+    fprintf(stdout, "Printing matrix %d x %d...\n", n, m);
+    int **matrix = (int **)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; ++i) {
+        matrix[i] = (int *) malloc(m * sizeof(int));
+    }
+
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; ++i) {
+            matrix[j][i] = 0;
+        }
+    }
+
+    for (int k = 0; k < nz; ++k) {
+        matrix[I[k]][J[k]] = 1;
+        matrix[J[k]][I[k]] = 1;
+    }
+
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; ++i) {
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
+}
+
+
 int main(int argc, char *argv[]){
-    int ret_code;
-    MM_typecode matcode;
+    MM_typecode matrixCode;
     FILE *f;
     int M, N, nz;
     int i, *I, *J;
-    double *val;
 
     if (argc < 2){
         fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
@@ -45,32 +47,26 @@ int main(int argc, char *argv[]){
         }
     }
 
-    if (mm_read_banner(f, &matcode) != 0){
+    if (mm_read_banner(f, &matrixCode) != 0){
         printf("Could not process Matrix Market banner.\n");
         exit(1);
     }
 
-
     /*  This is how one can screen matrix types if their application */
     /*  only supports a subset of the Matrix Market data types.      */
-
-    if (mm_is_complex(matcode) && mm_is_matrix(matcode) && mm_is_sparse(matcode) ){
+    if (mm_is_complex(matrixCode) && mm_is_matrix(matrixCode) && mm_is_sparse(matrixCode) ){
         printf("Sorry, this application does not support ");
-        printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
+        printf("Market Market type: [%s]\n", mm_typecode_to_str(matrixCode));
         exit(1);
     }
 
     /* find out size of sparse matrix .... */
-
-    if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &nz)) !=0)
+    if ((mm_read_mtx_crd_size(f, &M, &N, &nz)) !=0)
         exit(1);
 
-
-    /* reseve memory for matrices */
-
+    /* reserve memory for matrices */
     I = (int *) malloc(nz * sizeof(int));
     J = (int *) malloc(nz * sizeof(int));
-    val = (double *) malloc(nz * sizeof(double));
 
 
     /* NOTE: when reading in doubles, ANSI C requires the use of the "l"  */
@@ -78,21 +74,21 @@ int main(int argc, char *argv[]){
     /*  (ANSI C X3.159-1989, Sec. 4.9.6.2, p. 136 lines 13-15)            */
 
     for (i=0; i<nz; i++){
-        fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+        fscanf(f, "%d %d\n", &I[i], &J[i]);
         I[i]--;  /* adjust from 1-based to 0-based */
         J[i]--;
     }
 
-    if (f !=stdin) fclose(f);
+    if (f !=stdin)
+        fclose(f);
 
-    /************************/
-    /* now write out the first 100 lines of the matrix */
-    /************************/
-
-    mm_write_banner(stdout, matcode);
+    /* now write out the matrix */
+    mm_write_banner(stdout, matrixCode);
     mm_write_mtx_crd_size(stdout, M, N, nz);
-    for (i=0; i<100; i++)
-        fprintf(stdout, "%d %d %20.19g\n", I[i]+1, J[i]+1, val[i]);
+    for (i=0; i<nz; i++)
+        fprintf(stdout, "%d %d\n", I[i]+1, J[i]+1);
+
+    createSparseArray(I, J, M, N, nz);
 
     return 0;
 }
