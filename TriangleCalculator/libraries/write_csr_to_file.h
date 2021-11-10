d253a63f29de1b83db79986215c7ba15322fd4ff
Billkyriaf <bkyriafinis@gmail.com>
2021-11-10 23:12:30 +0200

    Files to write CSR matrix to .csr files

---
+++ TriangleCalculator/libraries/write_csr_to_file.h
@@ -0,0 +1,9 @@
+#ifndef TRIANGLECALCULATOR_WRITE_CSR_TO_FILE_H
+#define TRIANGLECALCULATOR_WRITE_CSR_TO_FILE_H
+
+#include "matrix_manipulation.h"
+
+void writeToFile(char *filename, CSR *matrix);
+
+
+#endif
